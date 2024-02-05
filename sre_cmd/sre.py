711d2117e5da2b74bf490386bd9a9938fbca509a
Michael Puskar <puskar@puskar.net>
2024-02-05 17:33:25 -0500

    skeleton in place

--- sre_cmd/sre.py
+++ sre_cmd/sre.py
@@ -10,8 +10,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("list", nargs="?", help="list deployments in all namespaces")
 parser.add_argument("--namespace", help="constrain deployments to this namespace")
 parser.add_argument("scale", nargs="?", help="set a new size for a replica or deployment")
-parser.add_argument("--replicas", type=int, help="new number of replicas")
+parser.add_argument("--replicas", help="new number of replicas")
 parser.add_argument("--deployment", help="deployment name")
+parser
 args = parser.parse_args()
 print(args)
 
@@ -19,10 +20,12 @@ def listdeploy(namespace):
     kubecmdargs = ["get",  "-A", "deployments"] if namespace == None else ["get", "deployments", "--namespace", namespace]
     subprocess.run([kubecmd] + kubecmdargs)
 
-def scale(replicas, deployment, namespace):
+def scale(replicas, namespace, deployment):
     kubecmdargs = ["scale", "--replicas", replicas, "--namespace", namespace, deployment]
-    #subprocess.run([kubecmd] + kubecmdargs)
-    print([kubecmd] + kubecmdargs)
+    subprocess.run([kubecmd] + kubecmdargs)
+
+def info():
+    
 
 
 if args.list == "list":
@@ -30,3 +33,6 @@ if args.list == "list":
     
 if args.list == "scale":
     scale(args.replicas, args.namespace, args.deployment)
+    
+if args.list == "info":
+    info()

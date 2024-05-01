74d9143ab8986853046c431451e011e6dd80e9bc
Michael Puskar <puskar@puskar.net>
2024-05-01 16:46:53 -0400

    Basic web functionality TODO: support arbitrary number of variables

--- teamcal.py
+++ teamcal.py
@@ -9,18 +9,32 @@ flask_app = Flask("ghsathletics")
 
 url = "https://www2.arbitersports.com/ICal/School/schedule.ics?id=cHHWgTXR%2b%2fvlYfnDlphDuQ%3d%3d"
 
-cal = Calendar.from_ical(requests.get(url).text)
 
-#print(cal.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
+@flask_app.route("/ghscal/<part1>/<part2>/<part3>", methods=['GET'])
 
-newcal = Calendar()
+def getcal(part1, part2, part3):
+    cal = Calendar.from_ical(requests.get(url).text)
 
-for component in cal.walk(name="VEVENT"):
-    if re.search(".*Boys Freshman*", component.get("description")) and re.search(".*Sport: Lacrosse*", component.get("description")):
-        component.get("description")
-        newcal.add_component(component)
-        #print(component.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
-           
-print(newcal.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
+    #print(cal.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
 
+    newcal = Calendar()
 
+    for component in cal.walk(name="VEVENT"):
+        if re.search('.*' + part1 + '*', component.get("description"), flags=re.I) and re.search('.*' + part2 + '*', component.get("description"), flags=re.I) and re.search('.*' + part3 + '*', component.get("description"), flags=re.I):
+            component.get("description")
+            newcal.add_component(component)
+            #print(component.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
+            
+    newcal.add('X-WR-CALNAME', f'{part1} {part2} {part3}')
+  
+
+    response = make_response(newcal.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
+    response.headers['Content-Type'] = 'text/calendar'
+    response.headers['Content-Disposition'] = f'inline; filename="{part1}{part2}{part3}.ics"'
+    return(response)
+
+#getcal("boys", "freshmen", "lacrosse")
+
+  
+if __name__ == "__main__":
+    flask_app.run(host="0.0.0.0", port=8080, debug=True)

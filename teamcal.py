0e3b2c5ce820fffd956b48020d70ca15b44b08ce
Michael Puskar <puskar@puskar.net>
2024-05-04 20:01:47 -0400

    Return 404 if no calendar components found

--- teamcal.py
+++ teamcal.py
@@ -1,45 +1,47 @@
 import requests
 from icalendar import Calendar, Event
-from flask import Flask, make_response, render_template
-
-
-flask_app = Flask("ghsathletics")
-
+from flask import Flask, abort, make_response, render_template
 
 url = "https://www2.arbitersports.com/ICal/School/schedule.ics?id=cHHWgTXR%2b%2fvlYfnDlphDuQ%3d%3d"
 
+flask_app = Flask("ghscal")
 
 @flask_app.route("/ghscal/", methods=['GET'], defaults={'part1': '', 'part2': '', 'part3': ''})
 @flask_app.route("/ghscal/<string:part1>", methods=['GET'], defaults={'part2': '', 'part3': ''})
 @flask_app.route("/ghscal/<string:part1>/<string:part2>", methods=['GET'], defaults= {'part3': ''})
 @flask_app.route("/ghscal/<string:part1>/<string:part2>/<string:part3>", methods=['GET'])
 
-
 def getcal(part1, part2, part3):
+    
     cal = Calendar.from_ical(requests.get(url).text)
 
     #print(cal.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
-
+    
     match = [part1, part2, part3]
     match = [element.lower() for element in match]
-
     newcal = Calendar()
+
+    comp_count=0
 
     for component in cal.walk(name="VEVENT"):
         desc = component.get("description").replace("\n", " ").lower().split()
-        
+
         #print(f'match={match} desc={desc}')
         if all(any(sub in string for string in desc) for sub in match):
             component.get("description")
             newcal.add_component(component)
-            
+            comp_count += 1        
+
     newcal.add('X-WR-CALNAME', f'{part1.title()} {part2.title()} {part3.title()}')
-  
 
     response = make_response(newcal.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
     response.headers['Content-Type'] = 'text/calendar'
     response.headers['Content-Disposition'] = f'inline; filename="{part1.title()}{part2.title()}{part3.title()}.ics"'
-    return(response)
+    
+    if comp_count == 0:
+        return(abort(404))
+    else:
+        return(response)
 
 #getcal("boys", "freshmen", "lacrosse")
 
